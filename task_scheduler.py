604395a6a2f183b282d618c304bc8a23de389bbb
Jhean Phabio <127798956+jhonnunes443@users.noreply.github.com>
2024-04-05 22:28:52 -0300

    Update task_scheduler.py
    This is a task scheduler file to upload files on python from the internet in addition executing in follow to educational proposes. You can change the code in agree with your proposes using a file's url, zip name´s file, path and  language to execute file.

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,52 +1,37 @@
-import schedule
-import time
-import datetime
+
+from pathlib import Path
 import subprocess
+import time
+import os
 
-tasks = [
-    {"name": "Task1", "max_runtime": 3, "command": r"python C:\Users\Usuario\Documents\'My files'\'Python projects'\keylogger\keylogger.py"},
-    {"name": "Task2", "max_runtime": 6, "command": r"exit()"},
-]
-
-
-def task1_actions():
-    try:
-        print("Task1: Starting action 1")
-        input("Press Enter to continue...")  
-    
-        print("Task1: Action 1 completed")
-        print("Task1: Starting action 2")
-        input("Press Enter to continue...") 
-
-        print("Task1: Action 2 completed")
-    except Exception as e:
-        print(f"Task1: Error executing actions: {str(e)}")
-if __name__ == "__main__":
-    task1_actions()
-
-def execute_task(task_name, max_runtime, command):
-    try:
-        start_time = datetime.datetime.now()
-
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        process.communicate()
-        exit_code = process.returncode
-        end_time = datetime.datetime.now()
-        runtime = (end_time - start_time).total_seconds() / 3600  
-        if runtime > max_runtime:
-            print(f"Task '{task_name}' exceeded max runtime ({max_runtime} hours) and was terminated.")
-            process.terminate()  
-        else:
-            print(f"Task '{task_name}' completed in {runtime:.2f} hours.")
-            if exit_code != 0:
-                print(f"Task '{task_name}' encountered an error. Exit code: {exit_code}")
-    except Exception as e:
-        print(f"Error executing task '{task_name}': {str(e)}")  
+url = "https://exemple.com"
+comand = f"wget {url}"
+unziping = "unzip main.zip"
+directory = "filefolder-main"
+execution = "python3 yourfile.py"
 
 
-for task in tasks:
-    schedule.every().minute.do(execute_task, task["name"], task["max_runtime"], task["command"])
+file = Path("file.py")
+file2 = Path("filefolder-main")
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)  # Check for scheduled tasks every minute
+try:
+    if not file.is_file():
+        print("File not exist. \n")
+        time.sleep(1)
+        print("Downloading file...\n\n") 
+        subprocess.run(comand, shell=True)
+        time.sleep(1)
+        print("Unziping file.\n")
+        subprocess.run(unziping, shell=True)
+        time.sleep(1)
+        print("Changing directory.")
+        os.chdir(directory) 
+        print("#Running script.#")
+        subprocess.run(execution, shell=True)
+    elif file2.is_file():
+        print("Changing directory.")
+        os.chdir(directory) 
+        print("#Running script.#")
+        subprocess.run(execution, shell=True)
+    else:
+        print("Failure to acess file.\n")
